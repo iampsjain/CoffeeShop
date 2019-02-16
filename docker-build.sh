1cddd721e9f3056627138cf59998f3311ae050c5
Darshan Jain <nnd.darshan@gmail.com>
2019-02-16 19:32:07 +0530

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,4 @@
-sudo docker rm -f javacontainer mysqlcontainer
-sudo docker build -t mysql-img  -f mysql-dockerfile .
-sudo docker run --name mysqlcontainer -d -p 3306:3306 mysql-img
+sudo docker rm -f javacontainer
 sudo mvn clean install
 sudo chmod -R 777 target
 sudo docker build -t java-img  -f java-dockerfile .
