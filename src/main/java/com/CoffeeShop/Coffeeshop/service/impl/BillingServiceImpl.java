4fa0aa931b1b7451209a84d5db46fa059a832bb9
iampsjain <iampsjain@gmail.com>
2019-02-25 09:47:26 +0530

    removed conflicts from billing service impl file

--- src/main/java/com/CoffeeShop/Coffeeshop/service/impl/BillingServiceImpl.java
+++ src/main/java/com/CoffeeShop/Coffeeshop/service/impl/BillingServiceImpl.java
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-package com.CoffeeShop.Coffeeshop.service.impl;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.CoffeShop.Coffeeshop.model.Address;
-import com.CoffeShop.Coffeeshop.model.Billing;
-import com.CoffeeShop.Coffeeshop.repo.AddressRepo;
-import com.CoffeeShop.Coffeeshop.repo.BillingRepo;
-import com.CoffeeShop.Coffeeshop.service.BillingService;
-
-@Service
-public class BillingServiceImpl implements BillingService {
-	@Autowired
-	BillingRepo billingRepo;
-
-	@Autowired
-	AddressRepo addressRepo;
-
-	@Override
-	public List<Billing> getBillings(int customer_id) {
-		// TODO Auto-generated method stub
-		return billingRepo.findAll();
-	}
-
-	@Override
-	public Address getBiilingAddress(int billingId) {
-		Billing bill = billingRepo.getOne(billingId);
-		Address address = addressRepo.getOne(bill.getAddressId());
-		return address;
-	}
-
-	@Override
-	public Billing addBill(Billing billing) {
-		return billingRepo.save(billing);
-	}
-
-	@Override
-	public Billing updateBill(Billing billing) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Billing> getAllbill() {
-		// TODO Auto-generated method stub
-		return billingRepo.findAll();
-	}
-
-}
-=======
 package com.CoffeeShop.Coffeeshop.service.impl;
 
 import java.util.List;
@@ -106,4 +52,3 @@ public class BillingServiceImpl implements BillingService {
 	}
 
 }
->>>>>>> 451ddff2d9d1d00bb2aa5f6d9578238f2dd6bbba
